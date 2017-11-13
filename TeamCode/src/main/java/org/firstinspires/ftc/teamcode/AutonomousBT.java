2c5fc25e75492ea4359198f265209414ebb08274
bk3468 <bk3468@gmail.com>
2017-11-12 16:19:29 -0800

    Added the autonomous Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBT.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by rootroot on 11/12/17.
+ */
+
+public class AutonomousBT extends LinearOpMode{
+    Hardware robot   = new Hardware();
+    @Override
+    public void runOpMode(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRT.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by rootroot on 11/12/17.
+ */
+
+public class AutonomousRT extends LinearOpMode{
+    Hardware robot   = new Hardware();
+    @Override
+    public void runOpMode(){
+
+    }
+}
